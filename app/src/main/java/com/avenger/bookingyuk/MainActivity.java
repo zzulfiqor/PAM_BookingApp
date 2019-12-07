6c249bc7fa3fdb039d062948d4aa040b20c9d43c
ffjr289 <fatimah.fajar@students.amikom.ac.id>
2019-12-07 20:27:16 +0700

    halo terserah

--- app/src/main/java/com/avenger/bookingyuk/MainActivity.java
+++ app/src/main/java/com/avenger/bookingyuk/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //tes
+    //terserah
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
