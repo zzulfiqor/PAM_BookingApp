c3885625c78db36fc9bc8dc7422b8b2929401e11
zzulfiqor <zzlfiqor@gmail.com>
2019-12-20 21:19:45 +0700

    add warna

--- app/src/main/java/com/avenger/bookingyuk/MainActivity.java
+++ app/src/main/java/com/avenger/bookingyuk/MainActivity.java
@@ -5,11 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //tes
-    //terserah
-    //mau aku booking ga?
-    //udah ni
-    //OYOdiskon
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
