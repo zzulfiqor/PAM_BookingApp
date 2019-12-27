025ecf02068f25982f7e525b10f1e7e193cfed98
zzulfiqor <zzlfiqor@gmail.com>
2019-12-27 15:24:32 +0700

    35% UI

--- app/src/main/java/com/avenger/bookingyuk/View/Activity/Description.java
+++ app/src/main/java/com/avenger/bookingyuk/View/Activity/Description.java
@@ -2,15 +2,29 @@ package com.avenger.bookingyuk.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.avenger.bookingyuk.R;
 
 public class Description extends AppCompatActivity {
 
+    Button btnPesan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
+        btnPesan = findViewById(R.id.btnPesan);
+        btnPesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), PesananFinal.class);
+                startActivity(i);
+            }
+        });
     }
 }
