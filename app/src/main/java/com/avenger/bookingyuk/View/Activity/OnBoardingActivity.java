89484e9a1f6e2ad5a0a70d8c0148314cf3e4ffa3
zzulfiqor <zzlfiqor@gmail.com>
2019-12-27 14:07:01 +0700

    fix size of image in onboarding

--- app/src/main/java/com/avenger/bookingyuk/View/Activity/OnBoardingActivity.java
+++ app/src/main/java/com/avenger/bookingyuk/View/Activity/OnBoardingActivity.java
@@ -3,7 +3,10 @@ package com.avenger.bookingyuk.View.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.avenger.bookingyuk.Adapter.OnBoardingPagerAdapter;
 import com.avenger.bookingyuk.R;
@@ -12,12 +15,22 @@ public class OnBoardingActivity extends AppCompatActivity {
 
     ViewPager onBoardingPager;
     OnBoardingPagerAdapter adapter;
+    Button btn_skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
         IntializeComponrnts();
+
+        btn_skip = findViewById(R.id.button_skip);
+        btn_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
 
         adapter = new OnBoardingPagerAdapter(getSupportFragmentManager());
         onBoardingPager.setAdapter(adapter);
