da2b11d58d946e22a0bb502a920b770d861fd93f
zzulfiqor <zzlfiqor@gmail.com>
2020-01-03 09:07:09 +0700

    commit

--- app/src/main/java/com/avenger/bookingyuk/View/Activity/ListRuangan.java
+++ app/src/main/java/com/avenger/bookingyuk/View/Activity/ListRuangan.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.avenger.bookingyuk.R;
 
 public class ListRuangan extends AppCompatActivity {
     TextView btnRoom1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_ruangan);
+
 
         btnRoom1 = findViewById(R.id.btnRoom1);
         btnRoom1.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +28,6 @@ public class ListRuangan extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
     }
 }
