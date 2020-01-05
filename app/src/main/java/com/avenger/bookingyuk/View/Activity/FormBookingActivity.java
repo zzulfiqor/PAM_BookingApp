641693b250f87695bc51f3d128bc002951f06b2b
zzulfiqor <zzlfiqor@gmail.com>
2020-01-06 00:59:04 +0700

    internet permission

--- app/src/main/java/com/avenger/bookingyuk/View/Activity/FormBookingActivity.java
+++ app/src/main/java/com/avenger/bookingyuk/View/Activity/FormBookingActivity.java
@@ -3,11 +3,13 @@ package com.avenger.bookingyuk.View.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.avenger.bookingyuk.R;
 
 public class FormBookingActivity extends AppCompatActivity {
 
+    EditText etOrganisasi, etAcara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
